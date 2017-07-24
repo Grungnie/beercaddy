a1a3a91efbe6e5363fbde10294b28f0cc453427e
Matthew <mbrown1508@outlook.com>
2017-07-24 23:49:09 +1000

    Added __init__.py

--- python/robot.py
+++ python/robot.py
@@ -7,15 +7,18 @@ class Robot(object):
         self.formatted_serial_data = []
 
     def set_motor_speed(self, left_motor_speed=None, right_motor_speed=None):
-        if -1028 < left_motor_speed < 1028:
+        if left_motor_speed is not None and -1028 < left_motor_speed < 1028:
             self.left_motor_speed = left_motor_speed
         else:
             raise(Exception('left motor ({}) is out of motor speed bounds. It has to be between -1028 and 1028'.format(left_motor_speed)))
 
-        if -1028 < right_motor_speed < 1028:
+        if right_motor_speed is not None and -1028 < right_motor_speed < 1028:
             self.right_motor_speed = right_motor_speed
         else:
             raise(Exception('right motor ({}) is out of motor speed bounds. It has to be between -1028 and 1028'.format(right_motor_speed)))
+
+        if left_motor_speed is None and right_motor_speed is None:
+            raise(Exception('No speed was set for either motor'))
 
     def send_data(self):
         self._format_data()
