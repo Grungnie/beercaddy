adfb9479586b44ade27c45427e85b559f27f7b5d
Matthew Brown <mbrown1508@outlook.com>
2017-07-28 00:29:32 +1000

    Added some test info

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 __author__ = 'Matthew'
 
+import requests
 from python import Robot
+import subprocess
 
 if __name__ == '__main__':
+    r = requests.get('https://raw.githubusercontent.com/Grungnie/microsoftbotframework/master/README.md')
+    print(r.text)
+
+    subprocess.call('touch imworking')
+
     robot = Robot()
 
     # start main loop
