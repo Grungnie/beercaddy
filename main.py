559efbb3575fe889f5af884f2b3cd51521496a56
Matthew Brown <mbrown1508@outlook.com>
2017-07-31 21:51:45 +1000

    Added write to file in loop

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import requests
 from python import Robot
 import subprocess
 import time
+import os
 
 if __name__ == '__main__':
-    r = requests.get('https://raw.githubusercontent.com/Grungnie/microsoftbotframework/master/README.md')
-
-    print(r.text)
-
-    subprocess.call('touch imworking')
-
     robot = Robot()
 
     # start main loop
-
+    counter = 0
     while True:
+        with open('test.txt', 'w+') as file:
+            file.write(str(counter))
         time.sleep(10)
+        counter += 1
