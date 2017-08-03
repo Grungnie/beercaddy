cdaa4de48e96b056e38871dd82a7e9c91f928d49
Matthew Brown <mbrown1508@outlook.com>
2017-08-04 01:17:23 +1000

    Moved serial to class

---
+++ arduinoserial.py
@@ -0,0 +1,83 @@
+import serial
+
+import logging
+logging.basicConfig(filename='../beercaddy.log', level=logging.INFO)
+
+
+class ArduinoSerial(object):
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600)
+
+    @staticmethod
+    def calcCheckSum(incoming):
+        msgByte = ArduinoSerial._Str2ByteArray(incoming)
+        check = 0
+        for i in msgByte:
+            check = ArduinoSerial._AddToCRC(i, check)
+        return check
+
+    @staticmethod
+    def _AddToCRC(b, crc):
+        b2 = b
+        if (b < 0):
+            b2 = b + 256
+        for i in range(8):
+            odd = ((b2^crc) & 1) == 1
+            crc >>= 1
+            b2 >>= 1
+            if (odd):
+                crc ^= 0x8C # this means crc ^= 140
+        return crc
+
+    @staticmethod
+    def _Str2ByteArray(msg):
+        b = bytearray()
+        b.extend(map(ord, msg))
+        logging.debug('ByteArray: {}'.format(b))
+        return b
+
+    def read_serial_message(self):
+        recieved_data = []
+
+        logging.debug('Waiting for message')
+        byte = self.ser.read()
+
+        if byte == b'\x3A':
+            length = int.from_bytes(self.ser.read(), byteorder='little')
+            command = int.from_bytes(self.ser.read(), byteorder='little')
+
+            for _ in range(length):
+                recieved_data.append(self.ser.read())
+
+            checksum = int.from_bytes(self.ser.read(), byteorder='little')
+
+            logging.debug('Message Length: {}'.format(length))
+            logging.debug('Message Command: {}'.format(command))
+            logging.debug('Raw Message {}'.format(recieved_data))
+            logging.debug('Message Checksum: {}'.format(checksum))
+
+        else:
+            logging.debug('Received unexpected character - {}'.format(int.from_bytes(byte, byteorder='little')))
+            return
+
+        try:
+            string_message = ''.join([x.decode('ascii') for x in recieved_data])
+            logging.debug('Message: {}'.format(string_message))
+        except UnicodeDecodeError:
+            logging.debug('Failed to decode message, UnicodeDecodeError')
+            return
+
+        checksum_string = ':' + chr(length) + chr(command) + string_message
+        calculated_checksum = self.calcCheckSum(checksum_string)
+        logging.debug('Calculated Checksum: {}'.format(calculated_checksum))
+
+        if checksum != calculated_checksum:
+            logging.debug('Checksum Failed')
+            return
+
+        if command == 1:
+            logging.info('Log Message - ' + string_message)
+        else:
+            logging.debug('Command Not Found')
+
+        return string_message
--- main.py
+++ main.py
@@ -1,96 +1,17 @@
 __author__ = 'Matthew'
 
-import requests
-from robot import Robot
-import time
-import serial
 import traceback
+from arduinoserial import ArduinoSerial
 
 import logging
 logging.basicConfig(filename='../beercaddy.log', level=logging.INFO)
 
-import binascii
-
-
-def calcCheckSum(incoming):
-    msgByte = Str2ByteArray(incoming)
-    check = 0
-    for i in msgByte:
-        check = AddToCRC(i, check)
-    return check
-
-
-def AddToCRC(b, crc):
-    b2 = b
-    if (b < 0):
-        b2 = b + 256
-    for i in range(8):
-        odd = ((b2^crc) & 1) == 1
-        crc >>= 1
-        b2 >>= 1
-        if (odd):
-            crc ^= 0x8C # this means crc ^= 140
-    return crc
-
-
-def Str2ByteArray(msg):
-    b = bytearray()
-    b.extend(map(ord, msg))
-    logging.debug('ByteArray: {}'.format(b))
-    return b
-
-
-def read_serial_message():
-    recieved_data = []
-
-    logging.debug('Waiting for message')
-    byte = ser.read()
-
-    if byte == b'\x3A':
-        length = int.from_bytes(ser.read(), byteorder='little')
-        command = int.from_bytes(ser.read(), byteorder='little')
-
-        for _ in range(length):
-            recieved_data.append(ser.read())
-
-        checksum = int.from_bytes(ser.read(), byteorder='little')
-
-        logging.debug('Message Length: {}'.format(length))
-        logging.debug('Message Command: {}'.format(command))
-        logging.debug('Raw Message {}'.format(recieved_data))
-        logging.debug('Message Checksum: {}'.format(checksum))
-
-    else:
-        logging.debug('Received unexpected character - {}'.format(int.from_bytes(byte, byteorder='little')))
-        return
-
-    try:
-        string_message = ''.join([x.decode('ascii') for x in recieved_data])
-        logging.debug('Message: {}'.format(string_message))
-    except UnicodeDecodeError:
-        logging.debug('Failed to decode message, UnicodeDecodeError')
-        return
-
-    checksum_string = ':' + chr(length) + chr(command) + string_message
-    calculated_checksum = calcCheckSum(checksum_string)
-    logging.debug('Calculated Checksum: {}'.format(calculated_checksum))
-
-    if checksum != calculated_checksum:
-        logging.debug('Checksum Failed')
-        return
-
-    if command == 1:
-        logging.info('Log Message - ' + string_message)
-    else:
-        logging.debug('Command Not Found')
-
-
 if __name__ == '__main__':
     try:
-        ser = serial.Serial('/dev/ttyACM0', 9600)
+        ser = ArduinoSerial()
 
         while True:
-            read_serial_message()
+            ser.read_serial_message()
 
     except Exception:
         logging.warning(traceback.format_exc())
