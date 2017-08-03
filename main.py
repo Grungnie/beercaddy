3eab1421279f46468f5d6cdb3c7e909aa928e71d
Matthew Brown <mbrown1508@outlook.com>
2017-08-03 22:09:40 +1000

    Added additional logging

--- main.py
+++ main.py
@@ -19,19 +19,22 @@ def read_serial_message():
         length = int.from_bytes(ser.read(), byteorder='big')
         command = int.from_bytes(ser.read(), byteorder='big')
 
+        logging.debug('Message Length: {}'.format(length))
+        logging.debug('Message Command: {}'.format(command))
+
         for _ in range(length):
             recieved_data.append(ser.read())
 
         checksum = ser.read()
 
     else:
-        logging.debug('Received unexpected character - {}'.format(byte.decode('ascii')))
+        logging.debug('Received unexpected character - {}'.format(int.from_bytes(byte, byteorder='big')))
         return
 
     string_message = ''.join([x.decode('ascii') for x in recieved_data])
 
     if checksum != b'\x01':
-        logging.debug('Checksum Failed - {} - {}'.format(chr(command) + string_message, checksum.decode('ascii')))
+        logging.debug('Checksum Failed - {} - {}'.format(command + string_message, checksum.decode('ascii')))
         return
 
     if command == 0:
