ef595633805926f20d45b7a1d64df0c96edef26b
Matthew Brown <mbrown1508@outlook.com>
2017-08-05 19:04:52 +1000

    changed to DEBUG

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from arduinoserial import ArduinoSerial
 from time import time, sleep
 
 import logging
-logging.basicConfig(filename='../beercaddy.log', level=logging.INFO)
+logging.basicConfig(filename='../beercaddy.log', level=logging.DEBUG)
 
 if __name__ == '__main__':
     try:
