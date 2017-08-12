6f88d1f66f51082d67231f63ddbe00838b32661b
Matthew Brown <mbrown1508@outlook.com>
2017-08-13 00:15:56 +1000

    Moved back to time

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ __author__ = 'Matthew'
 
 import traceback
 from arduinoserial import ArduinoSerial
-from time import time, sleep
-from datetime import datetime, timedelta
+from time import time
 
 import logging
 logging.basicConfig(filename='../beercaddy.log', level=logging.INFO)
@@ -12,16 +11,16 @@ if __name__ == '__main__':
     try:
         ser = ArduinoSerial()
 
-        last_command = datetime.now()
-        last_motor_command = datetime.now()
+        last_command = time()
+        last_motor_command = time()
         current_motor = 2
         last_motor = 5
 
         while True:
-            now = datetime.now()
+            now = time()
             ser.read_serial_message()
 
-            if last_motor_command < (now - timedelta(seconds=1)):
+            if last_motor_command < now - 1:
                 last_motor_command = now
 
                 last_motor = current_motor
@@ -30,12 +29,11 @@ if __name__ == '__main__':
                 if current_motor > 5:
                     current_motor = 2
 
-            if last_command < (now - timedelta(milliseconds=100)):
+            if last_command < now - 0.1:
                 last_command = now
 
                 ser.send_serial_message(bytearray([0]), last_motor)
                 ser.send_serial_message(bytearray([255]), current_motor)
-
 
 
     except Exception:
