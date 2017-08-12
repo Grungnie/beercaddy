e97c408f1c811372906ec4d7fa13e4bbff6b3c65
Matthew Brown <mbrown1508@outlook.com>
2017-08-12 23:20:27 +1000

    Moved to date time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ __author__ = 'Matthew'
 import traceback
 from arduinoserial import ArduinoSerial
 from time import time, sleep
+from datetime import datetime, timedelta
 
 import logging
 logging.basicConfig(filename='../beercaddy.log', level=logging.INFO)
@@ -11,16 +12,16 @@ if __name__ == '__main__':
     try:
         ser = ArduinoSerial()
 
-        last_command = time()
-        last_motor_command = time()
+        last_command = datetime.now()
+        last_motor_command = datetime.now()
         current_motor = 2
         last_motor = 5
 
         while True:
-            now = time()
+            now = datetime.now()
             ser.read_serial_message()
 
-            if last_motor_command < now - 1:
+            if last_motor_command < (now - timedelta(seconds=1)):
                 last_motor_command = now
 
                 last_motor = current_motor
@@ -29,20 +30,12 @@ if __name__ == '__main__':
                 if current_motor > 5:
                     current_motor = 2
 
-            if last_command < now - 1:
+            if last_command < (now - timedelta(milliseconds=100)):
                 last_command = now
 
                 ser.send_serial_message(bytearray([0]), last_motor)
                 ser.send_serial_message(bytearray([255]), current_motor)
 
-                ser.send_serial_message(bytearray([0]), last_motor)
-                ser.send_serial_message(bytearray([255]), current_motor)
-
-                ser.send_serial_message(bytearray([0]), last_motor)
-                ser.send_serial_message(bytearray([255]), current_motor)
-
-                ser.send_serial_message(bytearray([0]), last_motor)
-                ser.send_serial_message(bytearray([255]), current_motor)
 
 
     except Exception:
